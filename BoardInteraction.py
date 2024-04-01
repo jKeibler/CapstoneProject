fa6bea3892a314fab30cf1f1374cedf2daf4ca4a
josh <jkeibler92@southhills.edu>
2024-04-01 10:50:51 -0400

    First Interactive Python Script

---
+++ BoardInteraction.py
@@ -0,0 +1,32 @@
+#Code for hardware interaction
+#This just turns a green LED on and off
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+#Turning the LED off
+led.off()
+
+
+#Keeping a counter so it will eventually stop
+condition = True
+counter = 0
+
+#Keeping it in a loop so it will turn off and on
+while condition == True:
+    
+    led.on()
+    #sleeping for half a second so you can see it turn off and on
+    sleep(0.1)
+    led.off()
+    #sleeping for half a second so you can see it turn off and on
+    sleep(0.1)
+    counter += 1
+    print(int(counter))
+    
+    #Ending the program/stopping the flashing LED after a certain amount of time
+    if counter >= 100:
+        print(f"The LED has flashed {counter} times, program complete.")
+        condition = False
+        led.off()
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # CapstoneProject
-
-# Link for begining to learning Python -  https://www.youtube.com/watch?v=kqtD5dpn9C8 
-# Link for learning python functions - https://www.youtube.com/watch?v=u-OmVr_fT4s
